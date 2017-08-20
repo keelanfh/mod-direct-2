d8aff38f367d97a43fdc71ad269a68306c4c7e40
keelanfh <info@keelanfh.com>
2017-08-20 11:10:57 +0100

    Module list first test

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Flask-SQLAlchemy
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///froshims3.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///modules.db"
 app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 

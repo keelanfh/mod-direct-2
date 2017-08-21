5a7feab8fca957bd9b8a9bf6551b1b355bc1c0de
keelanfh <info@keelanfh.com>
2017-08-21 17:23:59 +0100

    update for all PSYC modules

--- download_html.py
+++ download_html.py
@@ -5,20 +5,22 @@ import urllib
 from application import Module
 import os
 
-modules = Module.querdatay.all()
+modules = Module.query.all()
+
+for module in modules:
+    if module.dept_code == "PSYC":
+        print module.url
 # TODO prevent this from asking for duplicates
-urls = [{"code": module.id, "url": module.machine_url} for module in modules if module.dept_code == "GEOG"]
-
-headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-results = []
-
+urls = [{"code": module.id, "url": module.machine_url} for module in modules if
+        module.dept_code == "PSYC" and module.machine_url is not None]
 
 # Save all the results into files.
+
 
 def html_output_file(module_code):
     return os.path.join(os.curdir, 'data', 'syllabus_html', module_code + '.html')
 
 
 for url in urls:
+    print url
     urllib.urlretrieve(url["url"], html_output_file(url["code"]))
-    break
